73d08238bd9197bb3c2cc6eb3904ced64a73fedf
yonghyunc <chyh9218@gmail.com>
2022-08-09 22:30:16 +0900

    Solve code_1945.py

--- code_1945.py
+++ code_1945.py
@@ -1,4 +1,5 @@
 # 1945. 간단한 소인수분해
+# 8/9 extra
 
 
 t = int(input())
@@ -6,13 +7,15 @@ t = int(input())
 for test_case in range(1, t + 1):
     num = int(input())
 
-    pf = [2, 3, 5, 7, 11]
-    div_count = [0] * 5
+    pf = [2, 3, 5, 7, 11]  # 소수 리스트
+    div_count = [0] * 5  # 소수 카운트 리스트
 
-    for idx, pf in enumerate(pf):
-        while num % pf == 0:
-            div_count[idx] += 1
+    for i, v in enumerate(pf):
+        while num % v == 0:  # 더 이상 해당 소수로 나눌 수 없을 때까지
+            div_count[i] += 1  # 나눌 때마다 카운트를 더해줌
+            num = num // v  # 다음 반복은 소수로 나눈 수로 수행
 
-    # print(*(div_count))
+    print(f'#{test_case}', end=' ')
+    print(*(div_count))
 
-    # print(f'#{test_case} {div_count}')
+    # print(f'#{test_case} *{div_count}')
---
+++ test.py
@@ -0,0 +1,8 @@
+coount = 0
+num = 1054
+while num % 2 == 0:
+    coount += 1
+    num = num // 2
+    print(num)
+
+print(coount)
