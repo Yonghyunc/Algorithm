957d1f9376fb3d97c119634e67c1acd61d8416cf
yonghyunc <chyh9218@gmail.com>
2022-08-18 01:11:37 +0900

    bad code and practice zip method

--- code_4861_1.py
+++ code_4861_1.py
@@ -3,7 +3,7 @@
 
 import sys
 
-sys.stdin = open('sample.txt')
+sys.stdin = open('sample_input.txt')
 
 t = int(input())
 
@@ -13,60 +13,70 @@ for test_case in range(1, t + 1):
     alp = [0] * n
 
     for i in range(n):
-        alp[i] = " ".join(input())
-        alp[i] = alp[i].split()
+        alp[i] = input()
 
-    alp2 = []
-    x = ''
-    for i in range(n):
-        for j in range(n):
-            x += alp[j][i]
-        alp2.append(x)
-        x = ''
+    a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    print(a[0:2][1])
+    print(a[1][1])
+    print(a[1][0:2])
 
-    c = m // 2
+    # print(alp)
+    # print(alp[1][0:4])
+    # print(alp[0:4])
+    # print(alp[0][1] + alp[1][1])
+    # print(alp[0:4][1])
 
-    # m 이 홀수
-    if m % 2 != 0:
-        # 가로 회문
-        for i in range(n):
-            for j in range(c, n - c):
-                if alp[i][j - c : j] == alp[i][j + c : j : -1]:
-                    print(f'#{test_case} ', *(alp[i][j - c : j + c + 1]), sep='')
-                    break
-        else:
-            for i in range(n):
-                for j in range(c, n - c):
-                    if alp2[i][j - c : j] == alp2[i][j + c : j : -1]:
-                        print(f'#{test_case} ', *(alp2[i][j - c : j + c + 1]), sep='')
-                        break
+    # alp2 = []
+    # x = ''
+    # for i in range(n):
+    #     for j in range(n):
+    #         x += alp[j][i]
+    #     alp2.append(x)
+    #     x = ''
 
-        # else:
-        #     for j in range(n):
-        #         for i in range(c, n - c):
-        #             if alp[i - c : i][j] == alp[i + c : i : -1][j]:
-        #                 print(f'#{test_case} ', *(alp[i - c : i + c + 1][j]), sep='')
-        #                 exit()
+    # c = m // 2
 
-    # m 이 짝수
-    else:
-        # 가로 회문
-        for i in range(n):
-            for j in range(c, n - c + 1):
-                if alp[i][j - c : j] == alp[i][j + c - 1 : j - 1 : -1]:
-                    print(f'#{test_case} ', *(alp[i][j - c : j + c]), sep='')
-                    break
+    # # m 이 홀수
+    # if m % 2 != 0:
+    #     # 가로 회문
+    #     for i in range(n):
+    #         for j in range(c, n - c):
+    #             if alp[i][j - c : j] == alp[i][j + c : j : -1]:
+    #                 print(f'#{test_case} ', *(alp[i][j - c : j + c + 1]), sep='')
+    #                 break
+    #     # else:
+    #     #     for i in range(n):
+    #     #         for j in range(c, n - c):
+    #     #             if alp2[i][j - c : j] == alp2[i][j + c : j : -1]:
+    #     #                 print(f'#{test_case} ', *(alp2[i][j - c : j + c + 1]), sep='')
+    #     #                 break
 
-        # 세로 회문
-        else:
-            for i in range(n):
-                for j in range(c, n - c + 1):
-                    if alp2[i][j - c : j] == alp2[i][j + c - 1 : j - 1 : -1]:
-                        print(f'#{test_case} ', *(alp2[i][j - c : j + c]), sep='')
-                        break
-        # else:
-        #     for j in range(n):
-        #         for i in range(c, n - c + 1):
-        #             if alp[i - c : i][j] == alp[i + c - 1 : i - 1 : -1][j]:
-        #                 print(f'#{test_case} ', *(alp[i - c : i + c][j]), sep='')
-        #                 exit()
+    #     else:
+    #         for j in range(n):
+    #             for i in range(c, n - c):
+    #                 if alp[i - c : i][j] == alp[i + c : i : -1][j]:
+    #                     print(f'#{test_case} ', *(alp[i - c : i + c + 1][j]), sep='')
+    #                     exit()
+
+    # # m 이 짝수
+    # else:
+    #     # 가로 회문
+    #     for i in range(n):
+    #         for j in range(c, n - c + 1):
+    #             if alp[i][j - c : j] == alp[i][j + c - 1 : j - 1 : -1]:
+    #                 print(f'#{test_case} ', *(alp[i][j - c : j + c]), sep='')
+    #                 break
+
+    #     # 세로 회문
+    #     else:
+    #         for i in range(n):
+    #             for j in range(c, n - c + 1):
+    #                 if alp2[i][j - c : j] == alp2[i][j + c - 1 : j - 1 : -1]:
+    #                     print(f'#{test_case} ', *(alp2[i][j - c : j + c]), sep='')
+    #                     break
+    #     # else:
+    #     #     for j in range(n):
+    #     #         for i in range(c, n - c + 1):
+    #     #             if alp[i - c : i][j] == alp[i + c - 1 : i - 1 : -1][j]:
+    #     #                 print(f'#{test_case} ', *(alp[i - c : i + c][j]), sep='')
+    #     #                 exit()
