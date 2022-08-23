a731c9d8bcdc65e0b9c19fe3426d0c9468fc1bdb
yonghyunc <chyh9218@gmail.com>
2022-08-23 16:55:45 +0900

    Solve code_1223

--- Stack2/code_1223.py
+++ Stack2/code_1223.py
@@ -1,21 +1,18 @@
 # 1223. [S/W 문제해결 기본] 6일차 - 계산기2
 # 8/22
 
-
 for test_case in range(1, 11):
-    t = int(input())  # 테스트 케이스 길이
-    middle = input()  # 중위표기법
-    back = ''  # 후위표기법
+    t = int(input())       # 테스트 케이스 길이
+    middle = input()       # 중위표기법
+    back = ''              # 후위표기법
     stack = []
 
     for token in middle:
         # token 이 연산자라면
         if token in '*+':
-
             # 곱셈 연산자 (무조건 우선)
             if token == '*':
                 stack.append(token)
-
             # 덧셈 연산자 (무조건 나중)
             elif token == '+':
                 while stack and stack[-1] != '+':
@@ -46,3 +43,4 @@ for test_case in range(1, 11):
                 stack.append(x * y)
 
     print(f'#{test_case} {stack[-1]}')
+
