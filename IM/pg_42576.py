acc80395591226e5c11382b8f1ebed8e7958b7c0
yonghyunc <chyh9218@gmail.com>
2022-10-10 17:00:38 +0900

    Solve pg_42576

--- IM/pg_42576.py
+++ IM/pg_42576.py
@@ -1,20 +1,30 @@
 # 프로그래머스 42576 : 완주하지 못한 선수
 
 
+# def solution(participant, completion):
+#     answer = ''
+#     k = 0
+#     while k < len(participant):
+#         if participant[k] in completion:
+#             completion.remove(participant[k])
+#         else:
+#             answer = participant[k]
+#             break
+#         k += 1
+#     return answer
+
+
 def solution(participant, completion):
-    answer = ''
-    k = 0
-    while k < len(participant):
-        if participant[k] in completion:
-            completion.remove(participant[k])
-        else:
-            answer = participant[k]
-            break
-        k += 1
-    return answer
+    participant.sort()
+    completion.sort()
+    for i in range(len(completion)):
+        if participant[i] != completion[i]:
+            return participant[i]
+    else:
+        return participant[-1]
 
 
-participant = ["mislav", "stanko", "mislav", "ana"]
-completion = ["stanko", "ana", "mislav"]
+participant = ["leo", "kiki", "eden"]
+completion = ["eden", "kiki"]
 
 print(solution(participant, completion))
