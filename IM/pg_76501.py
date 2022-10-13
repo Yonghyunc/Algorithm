a7dc01f92687b149d3fe45ad3f063c243ecfcca5
yonghyunc <chyh9218@gmail.com>
2022-10-13 23:14:40 +0900

    Solve pg_76501

---
+++ IM/pg_76501.py
@@ -0,0 +1,17 @@
+# 프로그래머스 76501. 음양 더하기
+
+def solution(absolutes, signs):
+    answer = 0
+
+    for i in range(len(absolutes)):
+        if signs[i]:
+            answer += absolutes[i]
+        else:
+            answer -= absolutes[i]
+
+    return answer
+
+
+absolutes = [4,7,12]
+signs = [True, False, True]
+print(solution(absolutes, signs))
