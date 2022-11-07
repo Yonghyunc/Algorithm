38c10de166400779075ca4f62bb25639554f3469
yonghyunc <chyh9218@gmail.com>
2022-11-08 01:13:08 +0900

    Solve bj_14501

--- bj_14501.py
+++ bj_14501.py
@@ -9,7 +9,7 @@
 1 5
 
 4
-1 5
+2 5
 4 15
 3 10
 1 5
@@ -18,16 +18,24 @@
 
 def talk(i, gain=0):
     global max_gain
+
+    # 현재 날의 상담을 끝낼 수 있을 때
     if i + plans[i] < n:
         talk(i + plans[i], gain + gains[i])
-        talk(i + 1, gain)
 
+    # 현재 날 시작하는 상담을 하면 N일을 다 채울 때
     elif i + plans[i] == n:
         if gain + gains[i] > max_gain:
             max_gain = gain + gains[i]
+
+    # 현재 날이 상담 마지막일 때, (현재 날부터 시작하는 상담은 수행하지 못함)
     else:
         if gain > max_gain:
             max_gain = gain
+
+    # 현재 날짜를 포함하지 않고 다음 날로 건너뜀
+    if i + 1 < n:
+        talk(i + 1, gain)
 
 
 n = int(input())
